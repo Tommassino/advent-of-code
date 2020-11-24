bc1da046b9f2be7bee78627d594aac5f088e3158
Tomas Witzany <tomas.witzany@blindspot.ai>
2020-11-24 12:13:46 +0000

    day 1

--- 2018/1/code.py
+++ 2018/1/code.py
@@ -1,14 +1,25 @@
 # Advent of code Year 2018 Day 1 solution
 # Author = witzatom
 # Date = December 2018
+from itertools import cycle
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
     input = input_file.read()
 
+numbers = [int(i) for i in input.split("\n")]
+solution = sum(numbers)
+
+print("Part One : " + str(solution))
 
 
-print("Part One : "+ str(None))
+def first_duplicate(numbers):
+    frequency = 0
+    seen = {frequency}
+    for i in cycle(numbers):
+        frequency += i
+        if frequency in seen:
+            return frequency
+        seen.add(frequency)
 
 
-
-print("Part Two : "+ str(None))
+print("Part Two : " + str(first_duplicate(numbers)))
