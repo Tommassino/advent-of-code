10a545757b7314ce2bdf03c893c97189f88f34a0
Tomas Witzany <tomas.witzany@blindspot.ai>
2020-11-26 20:12:35 +0000

    day 5

--- 2018/5/code.py
+++ 2018/5/code.py
@@ -2,13 +2,33 @@
 # Author = witzatom
 # Date = December 2018
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
     input = input_file.read()
 
 
+def react(polymer: str) -> str:
+    def reacts(c1: str, c2: str):
+        return c1.lower() == c2.lower() and c1.islower() != c2.islower()
 
-print("Part One : "+ str(None))
+    result = []
+    for c in polymer:
+        if result and reacts(result[-1], c):
+            result.pop()
+        else:
+            result.append(c)
+    return "".join(result)
 
 
+print("Part One : " + str(len(react(input))))
 
-print("Part Two : "+ str(None))
+from string import ascii_lowercase
+
+min_lenght = None
+min_polymer = None
+for c in ascii_lowercase:
+    fixed_polymer = react(input.replace(c, "").replace(c.upper(), ""))
+    if min_lenght is None or len(fixed_polymer) < min_lenght:
+        min_lenght = len(fixed_polymer)
+        min_polymer = fixed_polymer
+
+print("Part Two : " + str(min_lenght))
