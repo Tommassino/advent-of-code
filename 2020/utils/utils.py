b8298bf30a335358ab3a0d2d9979630e2a8342f6
Tomas Witzany <tomas.witzany@blindspot.ai>
2020-12-12 11:10:27 +0000

    2020 day 11

--- 2020/utils/utils.py
+++ 2020/utils/utils.py
@@ -1,4 +1,6 @@
+from functools import wraps
 from itertools import islice
+from time import time
 
 
 def window(seq, n=2):
@@ -9,3 +11,15 @@ def window(seq, n=2):
     for elem in it:
         result = result[1:] + (elem,)
         yield result
+
+
+def timed(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        start = int(round(time() * 1000))
+        result = f(*args, **kwds)
+        elapsed = int(round(time() * 1000)) - start
+        print("%s took %dms to finish" % (f.__name__, elapsed))
+        return result
+
+    return wrapper
