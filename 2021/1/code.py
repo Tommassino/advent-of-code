d66e730e9dd687bc6e681c679cdb6a69c1a07b6e
Tomas Witzany <tomas.witzany@blindspot.ai>
2021-12-01 11:02:59 +0100

    day 1 solution

--- 2021/1/code.py
+++ 2021/1/code.py
@@ -2,6 +2,18 @@
 # Author = witzatom
 # Date = December 2021
 
+from utils.utils import window
+
 def run(puzzle_input):
-    print(f"Part One : {None}")
-    print(f"Part Two : {None}")
+    puzzle_input = list(map(int, puzzle_input.split("\n")))
+    part_one = sum(
+        first < second
+        for first, second in window(puzzle_input)
+    )
+    print(f"Part One : {part_one}")
+
+    part_two = sum(
+        sum(first_window) < sum(second_window)
+        for first_window, second_window in window(window(puzzle_input, n=3))
+    )
+    print(f"Part Two : {part_two}")
