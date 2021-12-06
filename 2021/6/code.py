23ca1eb3a138b4c996b0000c60b78c5c1908927d
Tomas Witzany <tomas.witzany@blindspot.ai>
2021-12-06 07:32:56 +0100

    day 6

--- 2021/6/code.py
+++ 2021/6/code.py
@@ -1,7 +1,20 @@
 # Advent of code Year 2021 Day 6 solution
 # Author = witzatom
 # Date = December 2021
+from collections import Counter
 
 def run(puzzle_input):
-    print(f"Part One : {None}")
-    print(f"Part Two : {None}")
+    timers = list(map(int, puzzle_input.split(",")))
+    total_fish = sum(day_n(80, timers).values())
+    forever_fish = sum(day_n(256, timers).values())
+
+    print(f"Part One : {total_fish}")
+    print(f"Part Two : {forever_fish}")
+
+def day_n(n, starting_timers):
+    spawn_timer = Counter(starting_timers)
+    for day in range(n):
+        spawn_timer[day + 7] += spawn_timer[day]
+        spawn_timer[day + 9] += spawn_timer[day]
+        spawn_timer[day] = 0
+    return spawn_timer
