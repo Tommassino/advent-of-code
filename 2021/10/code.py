ed4da3c3ff945d42baf9d99ad5e1b53f7f037489
Tomas Witzany <tomas.witzany@blindspot.ai>
2021-12-11 17:39:27 +0100

    day 10

--- 2021/10/code.py
+++ 2021/10/code.py
@@ -2,6 +2,54 @@
 # Author = witzatom
 # Date = December 2021
 
+
 def run(puzzle_input):
-    print(f"Part One : {None}")
-    print(f"Part Two : {None}")
+    puzzle_input = puzzle_input.split("\n")
+
+    print(f"Part One : {part_one(puzzle_input)}")
+    print(f"Part Two : {part_two(puzzle_input)}")
+
+
+def part_one(puzzle_input):
+    return sum(validate(line) for line in puzzle_input)
+
+
+def part_two(puzzle_input):
+    autocomplete_scores = sorted(
+        list(filter(lambda x: x > 0, (complete(line) for line in puzzle_input)))
+    )
+    return autocomplete_scores[len(autocomplete_scores) // 2]
+
+
+opening = set("([{<")
+pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+invalid_points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+incomplete_points = {"(": 1, "[": 2, "{": 3, "<": 4}
+
+
+def validate(line):
+    stack = []
+    for c in line:
+        if c in opening:
+            stack.append(c)
+        else:
+            matching = stack.pop()
+            if pairs[matching] != c:
+                return invalid_points[c]
+    return 0
+
+
+def complete(line):
+    stack = []
+    for c in line:
+        if c in opening:
+            stack.append(c)
+        else:
+            matching = stack.pop()
+            if pairs[matching] != c:
+                return 0
+    score = 0
+    for c in reversed(stack):
+        score *= 5
+        score += incomplete_points[c]
+    return score
