f963cc06c809cc204da44864002d28d158537258
Tomas Witzany <tomas.witzany@blindspot.ai>
2021-12-13 12:32:07 +0100

    day 13

--- 2021/13/code.py
+++ 2021/13/code.py
@@ -1,7 +1,60 @@
 # Advent of code Year 2021 Day 13 solution
 # Author = witzatom
 # Date = December 2021
+from typing import Set, Tuple
+
 
 def run(puzzle_input):
-    print(f"Part One : {None}")
-    print(f"Part Two : {None}")
+    print(f"Part One : {part_one(puzzle_input)}")
+    print(f"Part Two :\n{part_two(puzzle_input)}")
+
+
+def part_one(puzzle_input):
+    coordinates, commands = puzzle_input.split("\n\n")
+    grid = Grid.parse(coordinates)
+    commands = list(command.split("=") for command in commands.split("\n"))
+    fold, coordinate = commands[0]
+    grid.fold(fold[-1], int(coordinate))
+    return len(grid.coordinates)
+
+
+def part_two(puzzle_input):
+    coordinates, commands = puzzle_input.split("\n\n")
+    grid = Grid.parse(coordinates)
+    commands = list(command.split("=") for command in commands.split("\n"))
+    for fold, coordinate in commands:
+        grid.fold(fold[-1], int(coordinate))
+    return grid
+
+
+class Grid:
+    dimension_map = {"x": 0, "y": 1}
+
+    def __init__(self, coordinates: Set[Tuple[int, int]]) -> None:
+        self.coordinates = coordinates
+
+    def __repr__(self) -> str:
+        height = max(y for _, y in self.coordinates) + 1
+        width = max(x for x, _ in self.coordinates) + 1
+        return "\n".join(
+            "".join("█" if (x, y) in self.coordinates else " " for x in range(width))
+            for y in range(height)
+        )
+
+    def fold(self, dimension, fold_coordinate):
+        dimension = self.dimension_map[dimension]
+        new_coordinates = set()
+        for coordinate in self.coordinates:
+            if coordinate[dimension] > fold_coordinate:
+                new_coordinate = list(coordinate)
+                new_coordinate[dimension] = (
+                    fold_coordinate - new_coordinate[dimension]
+                ) % fold_coordinate
+                new_coordinates.add(tuple(new_coordinate))
+            else:
+                new_coordinates.add(coordinate)
+        self.coordinates = new_coordinates
+
+    @staticmethod
+    def parse(input):
+        return Grid({tuple(map(int, line.split(","))) for line in input.split("\n")})
