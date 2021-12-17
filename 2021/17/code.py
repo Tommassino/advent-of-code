26822a69f61f4bf664d93327fce1ca80da1289fb
Tomas Witzany <tomas.witzany@blindspot.ai>
2021-12-17 10:28:31 +0100

    day 17 animation

--- 2021/17/code.py
+++ 2021/17/code.py
@@ -5,7 +5,7 @@
 import re
 from typing import NamedTuple
 import math
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from utils import timed
 
 
@@ -36,6 +36,7 @@ class TargetArea(NamedTuple):
 @timed
 def run(puzzle_input):
     target_area = parse(puzzle_input)
+    visualize(target_area)
     velocities = find_velocities(target_area)
     print(f"Part One : {max(velocities.values())}")
     print(f"Part Two : {len(velocities.keys())}")
@@ -74,3 +75,75 @@ def find_velocities(target_area: TargetArea):
             ),
         )
     )
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from tqdm.auto import tqdm
+
+def trajectory(velocity: Point2, target_area: TargetArea):
+    position = Point2(0, 0)
+    yield replace(position)
+    while position.x <= target_area.x_max and position.y >= target_area.y_min:
+        position.add(velocity)
+        yield replace(position)
+        if target_area.isin(position):
+            break
+        velocity.y -= 1  # gravity
+        velocity.x = max(0, velocity.x - 1)  # drag
+
+def visualize(target_area: TargetArea):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(10, 10)
+    velocities = find_velocities(target_area)
+    trajectories = list(
+        tuple(zip(*list((t.x, t.y) for t in trajectory(Point2(*velocity), target_area))))
+        for velocity in velocities.keys()
+    )
+    min_x = min(
+        min(t)
+        for t, _ in trajectories
+    )
+    max_x = max(
+        max(t)
+        for t, _ in trajectories
+    )
+    min_y = min(
+        min(t)
+        for _, t in trajectories
+    )
+    max_y = max(
+        max(t)
+        for _, t in trajectories
+    )
+
+    num_frames = max(
+        len(tr[0]) + 4
+        for tr in trajectories
+    )
+    with tqdm(total=num_frames) as pbar:
+        def animate_step(frame):
+            pbar.update(1)
+            ax.clear()
+            ax.set_xlim((min_x - 10, max_x + 10))
+            ax.set_ylim((min_y - 10, max_y + 10))
+            for x, y in trajectories:
+                ax.plot(x[:frame], y[:frame], alpha=0.1)
+            for x, y in trajectories:
+                vector_x = x[frame-2:frame]
+                vector_y = y[frame-2:frame]
+                if len(vector_x) != 2:
+                    continue
+                vector = np.array((vector_x[1] - vector_x[0], vector_y[1] - vector_y[0]))
+                size = np.linalg.norm(vector)
+                if size <= 0:
+                    continue
+                vector = vector / size * 10
+                ax.plot(
+                    [vector_x[1] - vector[0], vector_x[1]],
+                    [vector_y[1] - vector[1], vector_y[1]]
+                )
+                ax.scatter([vector_x[1]], [vector_y[1]], s=5, marker='X')
+
+        anim = animation.FuncAnimation(fig, animate_step, frames = num_frames, interval=50)
+        anim.save('trench.gif')
