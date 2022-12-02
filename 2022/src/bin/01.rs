a776f248b48588e80ea69a90c2e5e09db66abf36
Tomas Witzany <tomas.witzany@blindspot.ai>
2022-12-02 12:39:32 +0100

    day 1

---
+++ 2022/src/bin/01.rs
@@ -0,0 +1,40 @@
+use log::info;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let elf_caloric_cache: u32 = input.split("\n\n").map(|elf_info| {
+        elf_info.lines().map(|x| x.parse::<u32>().unwrap()).sum()
+    }).max().unwrap();
+    Some(elf_caloric_cache)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut elf_caloric_caches: Vec<u32> = input.split("\n\n").map(|elf_info| {
+        elf_info.lines().map(|x| x.parse::<u32>().unwrap()).sum()
+    }).collect();
+    elf_caloric_caches.sort();
+    elf_caloric_caches.reverse();
+    Some(elf_caloric_caches.iter().take(3).sum())
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(24000));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
